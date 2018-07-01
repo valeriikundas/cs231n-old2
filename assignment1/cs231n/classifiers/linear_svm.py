040facb18f078641c968e8abc4bf3b07679c86ff
valeriykundas <valeriykundas@gmail.com>
2018-07-01 21:46:32 +0300

    .

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -27,20 +27,24 @@ def svm_loss_naive(W, X, y, reg):
   loss = 0.0
   for i in range(num_train):
     scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
+    correct_class_score = scores[y[i]] 
     for j in range(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
-
+        dW[:, j] += X[i]
+        dW[:, y[i]] += -X[i]
+        
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += reg * 2 * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -69,7 +73,13 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  correct_class_scores = np.choose(y, scores.T)
+  margins = scores - correct_class_scores[:, np.newaxis] + 1
+  for i in range(X.shape[0]):
+    margins[i, y[i]] = 0
+  margins = margins * (margins > 0)
+  loss += np.sum(margins) / X.shape[0]
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +94,10 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  X_copy = X
+  X_copy[range(num_train), y] = 0
+  dW = X_copy.T.dot((margins > 0)) / num_train
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
